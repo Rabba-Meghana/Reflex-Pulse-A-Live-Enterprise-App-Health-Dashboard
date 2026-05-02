9e6b4f34e65d7f4cfd97d9bb3cca614ef9207026
Rabba-Meghana <rabba.meghana@gmail.com>
2026-05-02 04:13:45 +0000

    silence sitemap plugin warning

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,9 @@
 import reflex as rx
+from reflex_base.plugins.sitemap import SitemapPlugin
 
 config = rx.Config(
     app_name="reflex_pulse",
     db_url="sqlite:///pulse.db",
     telemetry_enabled=False,
+    plugins=[SitemapPlugin()],
 )
